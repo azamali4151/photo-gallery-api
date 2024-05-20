50d6eb40cae25dfac31f2894271bf6521bf93482
Azam Ali <azamalibd808@gmail.com>
2024-05-20 22:54:08 +0600

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> _photos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPhotos();
+  }
+
+  Future<void> fetchPhotos() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+    if (response.statusCode == 200) {
+      setState(() {
+        _photos = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load photos');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Gallery App'),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: _photos.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: _photos.length,
+        itemBuilder: (context, index) {
+          final photo = _photos[index];
+          return ListTile(
+            leading: SizedBox(
+              width: 80,
+              height: 80,
+              child: Image.network(photo['thumbnailUrl'],width: 100,height: 100,fit: BoxFit.cover,),
+            ),
+            title: Text(photo['title']),
+          );
+        },
+      ),
+    );
+  }
+}
